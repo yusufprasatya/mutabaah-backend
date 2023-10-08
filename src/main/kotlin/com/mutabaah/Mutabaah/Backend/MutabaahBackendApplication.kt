9577e66ca81b15f741e8f7940d9bc7e6e2cf212b
yusufprasatya <yusufmarsudien10@gmail.com>
2023-10-08 10:05:40 +0700

    init boilerplate

---
+++ src/main/kotlin/com/mutabaah/Mutabaah/Backend/MutabaahBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.mutabaah.Mutabaah.Backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MutabaahBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<MutabaahBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/mutabaah/Mutabaah/Backend/MutabaahBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mutabaah.Mutabaah.Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MutabaahBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
